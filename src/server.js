91c8ea4c2d53e286a536233252cd5bf22517c981
hongvy0707 <hongvy0707@gmail.com>
2023-09-16 01:57:51 +0700

    Init Code

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
-
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
 const START_SERVER = () => {
   const app = express()
@@ -11,12 +11,11 @@ const START_SERVER = () => {
   const port = 8017
 
   app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
     res.end('<h1>Hello World!</h1><hr>')
   })
 
-  app.listen(port, hostname, () => {
-    console.log(`Hello Hongvy, I am running at http://${hostname}:${port}/`)
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
   })
 
   exitHook(() => {
